da161e68e8de76a1d2f6644a6d46aaca75af1bf8
AgusM1983 <pamalagutti1983@gmail.com>
2024-06-18 20:56:44 -0300

    Create mock.c

---
+++ mock.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "movimientos.h"
+#include <string.h>
+#include "time.h"
+#include <conio.h>
+
+void getDayMonthYeard(stMovimientos* M)
+{
+/// para limitar a la fecha actual
+
+    time_t tiempo_actual = time(NULL);
+    struct tm *tiempo_local = localtime(&tiempo_actual);
+    /// printf("Año: %d\n", tiempo_local->tm_year + 1900); // tm_year cuenta desde 1900
+    /// printf("Mes: %d\n", tiempo_local->tm_mon + 1); // tm_mon cuenta desde 0 (enero = 0)
+    /// printf("Día: %d\n", tiempo_local->tm_mday);
+
+
+    M->anio = ((tiempo_local->tm_year + 1900)-(rand()%100));
+
+    M->mes = (rand()%12)+1;
+    if(M->mes==4 || M->mes== 6|| M->mes==9 || M->mes==11)
+    {
+        M->dia= (rand()%30)+1;
+    }
+    if(M->mes==2)
+    {
+        if (esBisiesto(M->anio)==0)
+        {
+            M->dia = (rand()%29)+1;
+        }
+        M->dia= (rand()%28)+1;
+    }
+    else
+    {
+        M->dia = (rand()%31)+1;
+    }
+
+}
+
+void idCuenta(stMovimientos* M, char nombreArchivoCu[])
+{
+
+    M->idCuenta = rand()%(cuentaElementosArchivo(nombreArchivoCu)+1);
+}
+
+void eliminado(stMovimientos* M)
+{
+    M->eliminado = (rand()%2);
+}
+
+void importeydetalle(stMovimientos* M)
+
+{
+    char detallePositivos[10][100] = {"Mensaje +1","Mensaje +2","Mensaje +3","Mensaje +4","Mensaje +5","Mensaje +6","Mensaje +7","Mensaje +8","Mensaje +9","Mensaje +10"};
+    char detalleNegatitivos[10][100] = {"Mensaje -1","Mensaje -2","Mensaje -3","Mensaje -4","Mensaje -5","Mensaje -6","Mensaje -7","Mensaje -8","Mensaje -9","Mensaje -10"};
+    int numAzar;
+    numAzar = rand()%10;
+//generar importe entre -300mil y 300mil
+    if (numAzar%2==0) // si es par genera positivos
+    {
+        M->importe = (rand() % 300001);
+    }
+    else // si es impar genera negativos
+    {
+        M->importe = -1*(rand() % 300001);
+    }
+
+//generar detalle
+    if(M->importe > 0)
+    {
+        strcpy(M->detalle, detallePositivos[numAzar]); /// hacer un generador al azar
+    }
+    if(M->importe==0)
+    {
+        strcpy(M->detalle, "Movimiento nulo");
+    }
+    if(M->importe<0)
+    {
+        strcpy(M->detalle, detalleNegatitivos[numAzar]); /// hacer un generador al azar
+    }
+}
+
+void mockArchivo (char nombreArchivoMov[], char nombreArchivoCu[], int cantidad)
+{
+    int i;
+    for (i=0; i<cantidad; i++)
+    {
+        stMovimientos movimiento;
+        getDayMonthYeard(&movimiento);
+        idCuenta(&movimiento,nombreArchivoCu);
+        importeydetalle(&movimiento);
+        eliminado(&movimiento);
+        if(movimiento.eliminado==0)
+        {
+            buscarcuentaymodificarsaldo(nombreArchivoCu,movimiento.idCuenta,movimiento.importe);
+        }
+        movimiento.id=cuentaElementosArchivo(nombreArchivoMov)+1;
+        cargarUnMovimientoArchivo(nombreArchivoMov,nombreArchivoCu,movimiento);
+    }
+}
+
