32ee51e956102879b9e24bd6887c4500893697c5
AgusM1983 <pamalagutti1983@gmail.com>
2024-06-21 13:22:05 -0300

    Update movimientos.h

--- movimientos.h
+++ movimientos.h
@@ -1,6 +1,5 @@
 #ifndef MOVIMIENTOS_H_INCLUDED
 #define MOVIMIENTOS_H_INCLUDED
-#include "cuenta.h"
 
 typedef struct {
 
@@ -15,26 +14,13 @@ int eliminado; /// 0 si está activo - 1 si está eliminado
 
 }stMovimientos;
 
-
-stMovimientos cargaManualMovimiento(char ArchivoCuentas[]);
-void mostrarUnMovimiento(stMovimientos movimiento);
+stMovimientos cargaManualMovimiento(char ArchivoCuentas[], char ArchivoCliente[]);
 void cargarUnMovimientoArchivo(char ArchivoMovimientos[], char ArchivoCuentas[], stMovimientos movimiento);
-void mostrarArchivoMovimiento(char ArchivoMovimientos[]);
-int cuentaElementosArchivo(char ArchivoMovimientos[]);
-void buscarImprimirMovimientoxId(char ArchivoMovimiento[], int IdMovimiento);
-int pasarArchivoMovimientoaArregloMovimiento(char ArchivoMovimientos[], stMovimientos arreglo[]);
-stMovimientos* archivoCompleto2arregloMovimiento(char nombreArchivo[], int* v); ///pasa a puntero arreglo
-void insertarEnArreglodeMovimientoOrdenado(stMovimientos arreglo[], stMovimientos movimiento, int cantidadDeMovimiento);
-void ordenarEnArreglodeMovimientoxCuenta(stMovimientos arreglo[], int validos);
-void ordenarxCuenta(char ArchivoMovimientos[], stMovimientos arreglo[], int * validos);
+void mostrarEncabezadoMovimiento();
+void mostrarUnMovimiento(stMovimientos movimiento);
+void mostrarArchivoMovimiento(char ArchivoMovimientos[],int i);
+void buscarImprimirMovimientoxId(char ArchivoMovimientos[], int IdMovimiento);
 void buscarImprimirMovimientoxfecha(char ArchivoMovimientos[], int anioi, int mesi, int diai, int aniof, int mesf, int diaf);
-float buscarMayorImporteArchivoDeMovimientos(char ArchivoMovimiento[]);
-void mostrarxModificarMovimiento(stMovimientos arregloMovimiento[]); /// arregloMovimiento[0] original, arregloMovimiento[1] resultado
-void modificarRegistroDeUnMovimiento(char ArchivoMovimientos[], int numeroDeRegistro);
-
-int esBisiesto(int anio);
-
-void invierteOrdenArchivoMovimientos(char nombreDelArchivoMovimiento []);
-stMovimientos* buscarMovimientoPorId(char nombreArchivoMovimiento[], int Id);
-
+void modificarRegistroDeUnMovimiento(char ArchivoMovimientos[], int numeroDeRegistro, char nombreArchivoCu[]);
+void mostrarxModificarMovimiento(stMovimientos arregloMovimiento[]);/// el arreglo es de dos movimientos [0] original y [1]devuelve
 #endif // MOVIMIENTOS_H_INCLUDED
