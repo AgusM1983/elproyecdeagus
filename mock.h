f096b5fb9ae4080bc7c740b8f32f639666c02a43
AgusM1983 <pamalagutti1983@gmail.com>
2024-06-21 13:22:38 -0300

    Update mock.h

--- mock.h
+++ mock.h
@@ -1,13 +1,16 @@
 #ifndef MOCK_H_INCLUDED
 #define MOCK_H_INCLUDED
-#include "movimientos.h"
-#include "cuenta.h"
 
+/// cliente
+void cargaMock80Cliente(char nombreCliente[]);
+/// cuenta
+void cargaMock80Cuenta(char nombre[]);
+
+/// movimiento
 void getDayMonthYeard(stMovimientos* M);
 void idCuenta(stMovimientos* M, char nombreArchivoCu[]);
 void eliminado(stMovimientos* M);
 void importeydetalle(stMovimientos* M);
 void mockArchivo (char nombreArchivoMov[], char nombreArchivoCu[], int cantidad);
-
 
 #endif // MOCK_H_INCLUDED
