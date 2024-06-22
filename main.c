3e9da9574af9cf371e81486a7510d966b0fe0ef7
AgusM1983 <pamalagutti1983@gmail.com>
2024-06-21 21:09:43 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,19 +3,15 @@
 #include <conio.h>
 #include <time.h>
 #include <string.h>
-#include "generales.h"
-#include "cliente.h"
-#include "cuenta.h"
-#include "movimientos.h"
+#include "general.h"
 
-int main() {
-    /// eleccion de menu y clave??
-    menuEmpleados();
-    menuClientes();
+void menuEmpleados();
 
-    return 0;
+int main() 
+{
+menuEmpleados();
+return 0;
 }
-
 
 
 void menuEmpleados() {
@@ -23,6 +19,12 @@ void menuEmpleados() {
     char archivoClientes[] = "clientes.dat";
     char archivoCuentas[] = "cuentas.dat";
     char archivoMovimientos[] = "movimientos.dat";
+    stCliente cliente;
+    stMovimientos movimiento;
+    stCuentas cuenta;
+    int idCliente;
+    int idCuenta;
+    int idMovimiento;
 
     do {
         printf("\nMenu Empleados:\n");
@@ -40,7 +42,7 @@ void menuEmpleados() {
         printf("12. Baja Movimiento\n"); /// usar funcion modificar movimiento pero solo de 0 a 1
         printf("13. Modificar Movimiento\n"); /// tengo la funcion
         printf("14. Consultar Movimiento\n"); /// tengo la funcion
-        printf("15. Listar Movimientos por Cuenta\n");  
+        printf("15. Listar Movimientos por Cuenta\n");
         printf("16. Listar Movimientos por Fecha\n"); /// tengo la funcion
         printf("17. Cargar Movimientos Aleatorios\n"); /// mock ver el tema de las cuotas
         printf("18. Proceso de salvar datos\n");
@@ -49,58 +51,150 @@ void menuEmpleados() {
         printf("Seleccione una opcion: ");
         scanf("%d", &opcion);
 
+
         switch (opcion) {
             case 1:
+                cliente=cargaUnCliente();
+                cargaClienteEnArchivo(archivoClientes, cliente);
                 // Función para alta de cliente
                 break;
             case 2:
+                idCliente=ingresarDniClienteValido(archivoClientes);
+                if(idCliente!=-2)
+                {
+
+                }
+
                 // Función para baja de cliente
                 break;
             case 3:
+                idCliente=ingresarDniClienteValido(archivoClientes);
+                if(idCliente!=-2)
+                {
+                modificaClientePorId(archivoClientes,idCliente);
+                }                
                 // Función para modificar cliente
                 break;
             case 4:
+                idCliente=ingresarDniClienteValido(archivoClientes);
+                if(idCliente!=-2)
+                {
+                muestraUnClienteId(archivoClientes,idCliente);                    
+                }
                 // Función para consultar cliente
                 break;
             case 5:
-                mostrarArchivoCliente(archivoClientes);
+                mostrarClientes(archivoClientes, cantidad);
+                // Función para consultar cliente
                 break;
             case 6:
+
+                cuenta=cargaUnaCuenta(archivoClientes);
+                cargaCuentaArch(archivoCuentas,cuenta);
                 // Función para alta de cuenta
                 break;
             case 7:
+                idCuenta=ingresarNroCuentaValido(archivoCuentas);
+                if(idCuenta!=-2)
+                {
+                cambiarEstadoCuenta(archivoCuentas, idCuenta,1);
+                }
                 // Función para baja de cuenta
                 break;
             case 8:
+                idCuenta=ingresarNroCuentaValido(archivoCuentas);
+                if(idCuenta!=-2)
+                {
+                modificarCuenta(archivoCuentas, idCuenta);
+                }
                 // Función para modificar cuenta
                 break;
             case 9:
+                idCuenta=ingresarNroCuentaValido(archivoCuentas);
+                if(idCuenta!=-2)
+                {
+                muestraPorIdCuenta(archivoCuentas,idCuenta);
+                }
+
                 // Función para consultar cuenta
                 break;
             case 10:
+                idCliente=ingresarDniClienteValido(archivoClientes);
+                if(idCliente!=-2)
+                {
+                mostrarCuentasxIdCliente(archivoCuentas,idCliente);
+                }
                 // Función para listar cuentas por cliente
                 break;
+            
+            /// movimientos
+            
             case 11:
+                movimiento=cargaManualMovimiento(archivoCuentas);
+                cargarUnMovimientoArchivo(archivoMovimientos,archivoCuentas,movimiento);
                 // Función para alta de movimiento
                 break;
             case 12:
+                idCuenta=ingresarNroCuentaValido(archivoCuentas);
+                if(idCuenta!=-2)
+                {
+                muestraMovimientoPorNroCuenta(archivoMovimientos,idCuenta,30)
+                }
+                
+                idMovimiento=ingresarIdMovValido(archivoMovimientos);
+                if(idCuenta!=-2)
+                {
+                modificarEstadoIdMovimiento(archivoMovimientos, idMovimiento,1);
+                }
                 // Función para baja de movimiento
                 break;
             case 13:
+                idCuenta=ingresarNroCuentaValido(archivoCuentas);
+                if(idCuenta!=-2)
+                {
+                muestraMovimientoPorNroCuenta(archivoMovimientos,idCuenta,30)
+                }
+                
+                idMovimiento=ingresarIdMovValido(archivoMovimientos);
+                if(idMovimiento!=-2)
+                {
+                modificarRegistroDeUnMovimiento(archivoMovimientos,idMovimiento,archivoCuentas);
+                }
+                
                 // Función para modificar movimiento
                 break;
-            case 14:
+            case 14: /// no tiene sentido
+                
                 // Función para consultar movimiento
                 break;
             case 15:
+                idCuenta=ingresarNroCuentaValido(archivoCuentas);
+                if(idCuenta!=-2)
+                {
+                muestraMovimientoPorNroCuenta(archivoMovimientos,idCuenta,30)
+                }
                 // Función para listar movimientos por cuenta
                 break;
             case 16:
+                idCuenta=ingresarNroCuentaValido(archivoCuentas);
+                if(idCuenta!=-2)
+                {
+                muestraMovimientoPorNroCuentaPorFecha(archivoMovimientos,idCuenta,anio0,mes0,dia0,aniof,mesf,diaf,30)
+                }
+                
                 // Función para listar movimientos por fecha
                 break;
-            case 17:
+            case 17: /// no tiene sentido
+                
                 // Función para cargar movimientos aleatorios
                 break;
+            case 18: /// no tiene sentido
+                printf("18. Proceso de salvar datos\n");
+ 
+                // Función para cargar movimientos aleatorios
+                break;
+            case 19: /// no tiene sentido                
+            printf("19. Proceso cargar cuotas mensuales datos\n");
             case 0:
                 printf("Saliendo...\n");
                 break;
@@ -109,55 +203,3 @@ void menuEmpleados() {
         }
     } while (opcion != 0);
 }
-
-void menuClientes() {
-    int opcion;
-    char archivoClientes[] = "clientes.dat";
-    char archivoCuentas[] = "cuentas.dat";
-    char archivoMovimientos[] = "movimientos.dat";
-    char dni[10];
-    int idCliente, idCuenta;
-
-    printf("Ingrese su DNI: ");
-    scanf("%s", dni);
-    idCliente = traducirDniaIdCliente(archivoClientes, dni);
-
-    if (idCliente < 0) {
-        printf("DNI no encontrado o múltiples clientes con el mismo DNI\n");
-        return;
-    }
-
-    do {
-        printf("\nMenu Clientes:\n");
-        printf("1. Consultar mis datos\n");
-        printf("2. Listar mis cuentas\n");
-        printf("3. Listar movimientos de una cuenta\n");
-        printf("4. Buscar movimientos por fecha\n");
-        printf("0. Salir\n");
-        printf("Seleccione una opcion: ");
-        scanf("%d", &opcion);
-
-        switch (opcion) {
-            case 1:
-                // Función para consultar datos del cliente
-                break;
-            case 2:
-                mostrarCuentasxIdCliente(archivoCuentas, idCliente);
-                break;
-            case 3:
-                printf("Ingrese el ID de la cuenta: ");
-                scanf("%d", &idCuenta);
-                // Función para listar movimientos de una cuenta
-                break;
-            case 4:
-                // Función para buscar movimientos por fecha
-                break;
-            case 0:
-                printf("Saliendo...\n");
-                break;
-            default:
-                printf("Opción no válida\n");
-        }
-    } while (opcion != 0);
-}
-
